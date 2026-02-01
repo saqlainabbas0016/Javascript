d8d4d8a6ca99ca4658b5d384525197d67b3f6ad9
Saqlain iqbal <155665033+saqlainabbas0016@users.noreply.github.com>
2026-02-01 06:48:32 +0000

    today we study abiut variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12344
+let accountEmail = "saqlainabbas0016@gamil.com"
+var accountpassword = "1234"
+accountCity = "gilgit"
+let accountState;
+
+// accountId = "1234" not allow
+accountEmail = "saquu@gmail.com"
+accountpassword = "45991"
+accountCity = "danyour" 
+
+
+console.log(accountId)
+console.table([accountEmail,accountpassword,accountCity, accountState])
