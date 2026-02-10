81b45f1c22c9efab2d77bc5157e1a3ea67f3c256
Saqlain iqbal <155665033+saqlainabbas0016@users.noreply.github.com>
2026-02-10 07:39:17 +0000

    notes alsoo add in conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,68 @@
-let score = "33"
+let score = "saqlain"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
-console.log(typeof score);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 
-let thevalueInNumber = Number (score)
+let isLoggedIn = "hitesh"
 
-console.log(typeof thevalueInNumber);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
 
-console.log(thevalueInNumber)
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
 
-// notes
-// let score = "33abc"
-// let score = true  1
-// let score = false  0 
-// let score = undefined 
-// let score = null 0
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
