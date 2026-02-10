c68fd96abf3f0fda8db397cf3847f30bcc621b3a
Saqlain iqbal <155665033+saqlainabbas0016@users.noreply.github.com>
2026-02-10 07:37:39 +0000

    operations

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -15,7 +15,7 @@ let isLoggedIn = false
 let state;
 
 // number => 2 to power 53
-// bigint
+// bigint----
 // string => ""
 // boolean => true/false
 // null => standalone value
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+
+console.log(typeof score);
+
+let thevalueInNumber = Number (score)
+
+console.log(typeof thevalueInNumber);
+
+console.log(thevalueInNumber)
+
+// notes
+// let score = "33abc"
+// let score = true  1
+// let score = false  0 
+// let score = undefined 
+// let score = null 0
